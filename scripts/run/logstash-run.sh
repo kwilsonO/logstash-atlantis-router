e7265727a5bafe3fae3bbb6fc0176dfca4d4a8f2
root <root@ip-10-144-206-29.ec2.internal>
2015-08-14 23:27:24 +0000

    added some simple checks and moved sincedb directory

--- scripts/run/logstash-run.sh
+++ scripts/run/logstash-run.sh
@@ -6,5 +6,13 @@ LOGSTASHDIR="${LOGSTASHPATH}/${LOGSTASHVER}"
 CONFIGDIR="${LOGSTASHPATH}/config-files"
 LOGPATH="/var/log/atlantis/logstash"
 
-rm "${LOGPATH}/*"
+export SINCEDB_DIR="$REPOPATH"
+
+if [ -e "${LOGPATH}/out.log" ] then
+	rm "${LOGPATH}/out.log"
+fi
+if [ -e "${LOGPATH}/err.log" ] then
+	rm "${LOGPATH}/err.log"
+fi
+
 $LOGSTASHDIR/bin/logstash -f "${CONFIGDIR}/${REPONAME}.conf" > $LOGPATH/out.log 2> $LOGPATH/err.log &
