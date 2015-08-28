103985ce02e76f97e89e073941742329d33d9d45
Kyle Wilson <kwilson@ooyala.com>
2015-08-28 11:58:57 -0700

    changed run script to capture instance data

--- scripts/run/logstash-run.sh
+++ scripts/run/logstash-run.sh
@@ -1,6 +1,22 @@
 LOGSTASHVER="logstash-1.5.3"
 REPONAME="logstash-atlantis-router"
 REPOPATH="/root/atlantis-analytics"
+
+#Instance Data gathering
+
+URL="http://169.254.169.254/latest/meta-data"
+INSTFULLHOST=$(curl ${URL}/hostname)
+INSTID=$(curl ${URL}/instance-id)
+INSTTYPE=$(curl ${URL}/instance-type)
+INSTLOCALHST=$(curl ${URL}/local-hostname)
+INSTLOCALIPV4=$(curl ${URL}/local-ipv4)
+INSTMACADDR=$(curl ${URL}/mac)
+INSTPUBHOST=$(curl ${URL}/public-hostname)
+INSTPUBIP=$(curl ${URL}/public-ipv4)
+INSTSECG=$(curl ${URL}/security-groups)
+CONFIGSTR="filter { mutate { add_field => { 'host-full-hostname' => '${INSTFULLHOST}'  } add_field => { 'host-inst-id' => '${INSTID}' } add_field => { 'host-inst-type' => '${INSTTYPE}' } add_field => { 'host-local-hostname' => '${INSTLOCALHST}' } add_field => { 'host-local-ipv4' => '${INSTLOCALIPV4}' } add_field => { 'host-mac-addr' => '${INSTMACADDR}' } add_field => { 'host-pub-hostname' => '${INSTPUBHOST}' } add_field => { 'host-pub-ipv4' => '${INSTPUBIP}' } add_field => { 'host-sec-groups' => '${INSTSECG}' }  } }"
+#Other Logstash
+
 LOGSTASHPATH="${REPOPATH}/${REPONAME}"
 LOGSTASHDIR="${LOGSTASHPATH}/${LOGSTASHVER}"
 CONFIGDIR="${LOGSTASHPATH}/config-files"
@@ -15,4 +31,4 @@ if [ -e "${LOGPATH}/err.log" ]; then
 	rm "${LOGPATH}/err.log"
 fi
 
-$LOGSTASHDIR/bin/logstash -f "${CONFIGDIR}/${REPONAME}.conf" > $LOGPATH/out.log 2> $LOGPATH/err.log &
+$LOGSTASHDIR/bin/logstash -e $CONFIGSTR -f "${CONFIGDIR}/${REPONAME}.conf" > $LOGPATH/out.log 2> $LOGPATH/err.log &
